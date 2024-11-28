399358546a3f3e04ea616af5d2730fd345973dfb
Mark Stacey <markjstacey@gmail.com>
2024-11-28 17:24:16 -0330

    Delete app tests

--- src/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import { render, screen } from '@testing-library/react';
-import App from './app';
-
-test('should render hello world', t => {
-  render(<App />);
-
-  t.not(screen.queryByText('Hello, world!'), null);
-});
